9dcd46cb90e2b92b0a2ca035ff208f575b5b0500
miguelMedinaCastro <miguelmedinadecastro@gmail.com>
2025-06-09 13:37:44 -0300

    commit

--- src/chat/network.py
+++ src/chat/network.py
@@ -1,7 +1,16 @@
 from ..lib import socket
-from ..config import ADDR
+from ..config import PORT
 
 def create_client():
+    server_ip = input("type IP server: ").strip()
+    
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(ADDR)
+
+    try:
+        client.connect((server_ip, PORT))
+    except ConnectionRefusedError:
+        print(f'[Error] not possible to connect a {server_ip}: {PORT}')
+        exit(1)
+
+    # client.connect(ADDR)
     return client
--- src/config.py
+++ src/config.py
@@ -5,7 +5,8 @@ from pathlib import Path
 env_path = Path(__file__).resolve().parent.parent / ".env"
 load_dotenv(dotenv_path=env_path)
 
-HOST = os.getenv("HOST")
+HOST_SERVER = os.getenv("HOST")
+HOST_CLIENT = os.getenv("")
 PORT = int(os.getenv("PORT"))
 FORMAT = 'utf-8'
 ADDR = (HOST, PORT)
