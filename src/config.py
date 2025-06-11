69519f99d5e5cfd9495adefa709636c0b36e43b9
miguelMedinaCastro <miguelmedinadecastro@gmail.com>
2025-06-11 13:42:05 -0300

    upd libs path

--- src/config.py
+++ src/config.py
@@ -1,11 +1,10 @@
-import os
-from dotenv import load_dotenv
-from pathlib import Path
+from .lib import os
+from .lib import load_dotenv
+from .lib import Path
 
 env_path = Path(__file__).resolve().parent.parent / ".env"
 load_dotenv(dotenv_path=env_path)
 
 HOST_SERVER = os.getenv("HOST")
-HOST_CLIENT = os.getenv("")
 PORT = int(os.getenv("PORT"))
 FORMAT = 'utf-8'
--- src/lib.py
+++ src/lib.py
@@ -3,4 +3,5 @@ import threading
 import os
 import time
 import sys
+from pathlib import Path
 from dotenv import load_dotenv
